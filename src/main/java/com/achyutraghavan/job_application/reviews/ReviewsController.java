3737161c5f10d176577f122586eb29af2709a1b4
Achyut <raghavanachyut@gmail.com>
2024-10-13 19:25:02 +0530

    getAllReviews functionality

--- src/main/java/com/achyutraghavan/job_application/reviews/ReviewsController.java
+++ src/main/java/com/achyutraghavan/job_application/reviews/ReviewsController.java
@@ -1,7 +1,14 @@
 package com.achyutraghavan.job_application.reviews;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies/{companyId}")
@@ -13,5 +20,8 @@ public class ReviewsController {
         this.reviewsService = reviewsService;
     }
 
-
+    @GetMapping
+    private ResponseEntity<List<Review>> getAllReviews(@PathVariable Long companyId) {
+        return new ResponseEntity<>(reviewsService.getAllReviews(companyId), HttpStatus.OK);
+    }
 }
--- src/main/java/com/achyutraghavan/job_application/reviews/impl/reviewsServiceImpl.java
+++ src/main/java/com/achyutraghavan/job_application/reviews/impl/reviewsServiceImpl.java
@@ -1,8 +1,26 @@
 package com.achyutraghavan.job_application.reviews.impl;
 
+import com.achyutraghavan.job_application.reviews.Review;
+import com.achyutraghavan.job_application.reviews.ReviewsRepo;
 import com.achyutraghavan.job_application.reviews.ReviewsService;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class reviewsServiceImpl implements ReviewsService {
+
+    private ReviewsRepo reviewsRepo;
+
+    public reviewsServiceImpl(ReviewsRepo reviewsRepo) {
+        this.reviewsRepo = reviewsRepo;
+    }
+
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        List<Review> reviews;
+        reviews = reviewsRepo.findByCompanyId(companyId);
+        return null;
+    }
 }
